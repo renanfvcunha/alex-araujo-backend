42bda9360149ae08f7e03967dcd666ce3ee4e765
Renan Cunha <rfvcunha@hotmail.com>
2021-07-08 16:24:15 -0300

    Adjusting sitePrincipal controller

--- api/site-principal/controllers/site-principal.js
+++ api/site-principal/controllers/site-principal.js
@@ -7,10 +7,4 @@ const { sanitizeEntity } = require('strapi-utils')
  */
 
 
-module.exports = {
-  async find(ctx) {
-    const entity = await strapi.services['site-principal'].find();
-    entity.quemSomos.textos.texto = entity.quemSomos.textos.texto.replace('src="/', `src="${strapi.config.get('server.url')}/`); // Here we modify the 'Content' and add your website "url" to it
-    return sanitizeEntity(entity, { model: strapi.models['site-principal'] });
-  },
-};
+module.exports = {};
